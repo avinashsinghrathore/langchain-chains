854fa74fb2dae0ce7b71e6fce04a36f6059d5fc1
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-06-08 01:24:50 +0530

    simple chain implemented

---
+++ simple_chain.py
@@ -0,0 +1,23 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V4-Pro", task="text-generation")
+
+prompt = PromptTemplate(
+    template="Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+model = ChatHuggingFace(llm = llm)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({"topic": "cricket"})
+
+print(result)
