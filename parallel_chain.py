97f3caa526b6137711875583254fbdb1ef6ffc79
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-06-08 14:44:39 +0530

    implemented parallel chian concepts

---
+++ parallel_chain.py
@@ -0,0 +1,59 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V4-Pro", task="text-generation")
+
+model1 = ChatHuggingFace(llm=llm)
+
+llm2 = HuggingFaceEndpoint(repo_id="meta-llama/Llama-3.1-8B-Instruct", task="text-generation")
+
+model2 = ChatHuggingFace(llm=llm2)
+
+prompt1 = PromptTemplate(
+    template="Generate short and simple notes from the following text \n {text}",
+    input_variables=["text"],
+)
+
+prompt2 = PromptTemplate(
+    template="Generate 5 short question answer from the following text \n {text}",
+    input_variables=["text"],
+)
+
+prompt3 = PromptTemplate(
+    template="Merge the provided notes and quiz into a single document \n notes -> {notes} and quiz -> {quiz}",
+    input_variables=["notes", "quiz"],
+)
+
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel(
+    {"notes": prompt1 | model1 | parser, "quiz": prompt2 | model2 | parser}
+)
+
+merge_chain = prompt3 | model1 | parser
+
+chain = parallel_chain | merge_chain
+
+text = """
+The main goal of SVM is to maximize the margin between the two classes. The larger the margin the better the model performs on new and unseen data.
+
+Key Concepts
+Hyperplane: A decision boundary separating different classes in feature space and is represented by the equation wx + b = 0 in linear classification.
+Support Vectors: The closest data points to the hyperplane, crucial for determining the hyperplane and margin in SVM.
+Margin: The distance between the hyperplane and the support vectors. SVM aims to maximize this margin for better classification performance.
+Kernel: A function that maps data to a higher-dimensional space enabling SVM to handle non-linearly separable data.
+Hard Margin: A maximum-margin hyperplane that perfectly separates the data without misclassifications.
+Soft Margin: Allows some misclassifications by introducing slack variables, balancing margin maximization and misclassification penalties when data is not perfectly separable.
+C: A regularization term balancing margin maximization and misclassification penalties. A higher C value forces stricter penalty for misclassifications.
+Hinge Loss: A loss function penalizing misclassified points or margin violations and is combined with regularization in SVM.
+Dual Problem: Involves solving for Lagrange multipliers associated with support vectors, facilitating the kernel trick and efficient computation.
+"""
+
+result = chain.invoke({"text": text})
+
+print(result)
