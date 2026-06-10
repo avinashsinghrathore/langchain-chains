9cd9bb8289a945660554977eabd00d82a90b98ca
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-06-10 13:22:47 +0530

    implement complex sequence runnable example

--- runnable_sequence.py
+++ runnable_sequence.py
@@ -10,14 +10,20 @@ prompt = PromptTemplate(
     template="Write a joke about {topic}", input_variables=["topic"]
 )
 
-llm = HuggingFaceEndpoint(repo_id="meta-llama/Llama-3.1-8B-Instruct", task="text-generation")
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct", task="text-generation"
+)
 
-model = ChatHuggingFace(llm = llm)
+model = ChatHuggingFace(llm=llm)
 
 parser = StrOutputParser()
 
-chain = RunnableSequence(prompt, model, parser)
+prompt1 = PromptTemplate(
+    template="Explain the following joke - {text}", input_variables=["text"]
+)
 
-result = chain.invoke({"topic":"golf"})
+chain = RunnableSequence(prompt, model, parser, prompt1, model, parser)
+
+result = chain.invoke({"topic": "Chess"})
 
 print(result)
